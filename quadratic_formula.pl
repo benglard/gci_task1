e3d78065bd804dbaab61de7dc1fafc33bf672889
Benji Englard <benjienglard@yahoo.com>
2011-11-22 20:25:33 -0500

    Program to find the real solutions to a quadratic equation.

---
+++ quadratic_formula.pl
@@ -0,0 +1,58 @@
+use Android;
+my $droid = Android->new();
+
+#purpose of program
+$droid->makeToast('This program computes the real solutions to a quadratic equation'.);
+
+/* get values for a, b, and c
+	which correspond to the coefficients of the 
+	quadratic equation.
+*/
+
+$a = $droid->dialogGetInput("Value for A", "Please enter a value for A:",
+'');
+$a = $a->{'result'};
+$b = $droid->dialogGetInput("Value for B", "Please enter a value for B:",
+'');
+$b = $b->{'result'};
+$c = $droid->dialogGetInput("Value for C", "Please enter a value for C:",
+'');
+$c = $c->{'result'};
+
+/* 
+	Algorithm for finding real solutions.
+*/
+
+if ($a == 0) 
+{
+	if ($b == 0)
+	{
+		if ($c != 0) {
+			$droid->makeToast('No solution');
+		}
+		else {
+			$droid->makeToast('All Real Numbers');
+		}
+	}
+	else {
+		$x = -1 * ($c/$b);
+		$droid->makeToast("The equation is linear and its solution is $x");
+	}
+}
+else {
+	$rad = ($b*$b) - (4*$a*$c);
+	
+	if ($rad<0) {
+		$droid->makeToast('There are no real solutions');
+	}
+	elsif ($rad == 0) {
+		$x = (-1) * ($b/(2*$a));
+		$droid->makeToast("One Solution: $x");
+	}
+	else {
+		$s = sqrt($rad);
+		$x1 = ((-1*$b) + $s)/(2*$a);
+		$x2 = ((-1*$b) + $s)/(2*$a);
+		$droid->makeToast("There are two solutions and they are: $x1 and $x2");
+	}
+}
